cf8d9519d086d90874e87bbefb51229677802bd3
Pushkar Alange <pushkarbalange14@gmail.com>
2026-09-16 14:36:37 +0530

    Implement program to find the first digit of a number

---
+++ B.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    while (n >= 10) {
+        n /= 10;
+    }
+
+    cout << "First digit = " << n;
+
+    return 0;
+}
